3f279bb3aa75444c7b8724c8d2914942cdbba231
Charles Patterson Cook <cook.char.p@gmail.com>
2019-01-30 15:52:06 -0500

    Added functions and adapted pascalTriLine() to use BigInts

--- README.md
+++ README.md
@@ -6,9 +6,9 @@ Module with various miscellaneous functions for classifications of whole numbers
 * `listDigits`: returns a list of the digits in a number `n` in descending order of magnitude
 * `isOdd`: returns `true` if an integer `n` is odd
 * `isEven`: returns `true` if an integer `n` is even
-* `isSquare`: returns `true` if an integer `n` is a square number
 * `isTriangle`: returns `true` if an integer `n` is a triangle number
-* `isCubic`: returns `true` if an integer `n` is a cubic number
+* `isSquare`: returns `true` if an integer `n` is a square number
+* `isCube`: returns `true` if an integer `n` is a cube number
 * `isPandigital`: returns `true` if an integer `n` is a strict pandigital number
 * `isHappy`: returns `true` if an integer is a happy number
 * `pascalTriLine`: returns a list of numbers from line `n` of Pascal's Triangle
--- ccNumFuncs.jl
+++ ccNumFuncs.jl
@@ -1,16 +1,80 @@
 module ccNumFuncs
-	export pascalTriLine
+	export listDigits, isOdd, isEven, isTriangle, isSquare, isCube, isPandigital, isHappy, pascalTriLine
+
+	function listDigits(n::Integer)
+		n == 0 ?
+			(return [0]) :
+			n < 0 ?
+				throw(ArgumentError("`n` must be positive.")) :
+				true
+		local digits = []
+		m = 0
+		while n >= 10 ^ m
+			m += 1
+		end
+		local k = n
+		while m > 0
+			push!(digits, div(k, 10 ^ (m - 1)) % 10)
+			m -= 1
+		end
+		digits
+	end
+
+	function isOdd(n::Integer)
+		n % 2 == 1
+	end
+
+	function isEven(n::Integer)
+		n % 2 == 0
+	end
+
+	function isTriangle(n::Integer)
+		local l = isqrt(2 * n)
+		l^2 + l == 2 * n
+	end
+
+	function isSquare(n::Integer)
+		isqrt(n) == sqrt(n)
+	end
+
+	function isCube(n::Integer)
+		try
+			Int(cbrt(n)) == cbrt(n)
+		catch
+			false
+		end
+	end
+
+	function isPandigital(n::Integer)
+		local digits = listDigits(n)
+		for d = 0:9
+			in(d, digits) ? continue : (return false)
+		end
+		true
+	end
+
+	function isHappy(n::Integer)
+		n == 1 ? (return true) : true
+		local unhappyNums = Array{Int64}(undef, 0)
+		local m = n
+		while m != 1
+			push!(unhappyNums, m)
+			m = mapreduce(x -> x^2, +, listDigits(m))
+			in(m, unhappyNums) ? (return false) : continue
+		end
+		true
+	end
 
 	function pascalTriLine(n::Integer)
 		n == 0 ?
-			(return [Int64(1)]) : 
+			(return [big(1)]) : 
 			n > 0 ? 
 				true : 
 				throw(ArgumentError("`n` must be positive."))
 		local prevLine = pascalTriLine(n - 1)
-		local line = [Int64(1)]
+		local line = [big(1)]
 		for i = 1:n - 1
-			push!(line, Int64(prevLine[i]) + Int64(prevLine[i + 1]))
+			push!(line, prevLine[i] + prevLine[i + 1])
 		end
 		push!(line, 1)
 		line
